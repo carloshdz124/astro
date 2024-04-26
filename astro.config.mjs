a7746f897acafa5cf0cd3182bd8ef3d42347c840
Carlos David Hernández Martínez <cdavid.hernandez@alumnos.udg.mx>
2024-04-25 22:11:06 -0600

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://carloshdz124.github.io',
-    base: 'astro',
+    base: 'https://github.com/carloshdz124/carloshdz124.github.io',
 });
