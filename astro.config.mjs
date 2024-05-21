09bd893376fba5c3233d5b4d1eb53edf0b8fbb68
Carlos David Hernández Martínez <70905581+carloshdz124@users.noreply.github.com>
2024-05-21 14:28:47 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,4 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://carloshdz124.github.io',
-    base: 'https://github.com/carloshdz124/carloshdz124.github.io',
 });
